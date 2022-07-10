2252f5d047d4701a3612a970860b1f06ad3f0b38
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-07-10 16:26:13 -0400

    Create box component for displaying tic tac toe

---
+++ src/components/Box.js
@@ -0,0 +1,15 @@
+export const Box = () => {
+    return (
+        <div className="box-grid">
+            <div className="box-cell">1</div>
+            <div className="box-cell">2</div>
+            <div className="box-cell">3</div>
+            <div className="box-cell">4</div>
+            <div className="box-cell">5</div>
+            <div className="box-cell">6</div>
+            <div className="box-cell">7</div>
+            <div className="box-cell">8</div>
+            <div className="box-cell">9</div>
+        </div>
+    )
+}
