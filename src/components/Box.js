1d219841551fe1d100fe003a142ed504f4988801
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-07-11 22:34:13 -0400

    Make the box show dynamic information

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,15 +1,15 @@
-export const Box = () => {
+export const Box = ({ onClick, cells }) => {
     return (
-        <div className="box-grid">
-            <div className="box-cell" id="top-left">X</div>
-            <div className="box-cell" id="top-middle">O</div>
-            <div className="box-cell" id="top-right">3</div>
-            <div className="box-cell" id="middle-left">4</div>
-            <div className="box-cell" id="middle-middle">5</div>
-            <div className="box-cell" id="middle-right">6</div>
-            <div className="box-cell" id="bottom-left">7</div>
-            <div className="box-cell" id="bottom-middle">8</div>
-            <div className="box-cell" id="bottom-right">9</div>
+        <div className="box-grid" onClick={onClick}>
+            <div className="box-cell" id="top-left">{cells[0][0]}</div>
+            <div className="box-cell" id="top-middle">{cells[0][1]}</div>
+            <div className="box-cell" id="top-right">{cells[0][2]}</div>
+            <div className="box-cell" id="middle-left">{cells[1][0]}</div>
+            <div className="box-cell" id="middle-middle">{cells[1][1]}</div>
+            <div className="box-cell" id="middle-right">{cells[1][2]}</div>
+            <div className="box-cell" id="bottom-left">{cells[2][0]}</div>
+            <div className="box-cell" id="bottom-middle">{cells[2][1]}</div>
+            <div className="box-cell" id="bottom-right">{cells[2][2]}</div>
         </div>
     )
 }
