2648b05e41dae6c7821c31d317e2158961a3d17d
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-07-11 22:33:51 -0400

    Implement a state to keep track of x and o

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import {useState, useEffect} from 'react';
 import { Box } from './components/Box';
 
 function App() {
+
+  const [cells, setCells] = useState([[], [], []]);
+
+  const handleClick = (event) => {
+    console.log(event.target.id);
+    if (event.target.id == "top-left") {
+      setCells([["X"], [], []]);
+    }
+  }
+
   return (
     <div className="container">
-      <Box />
+      <Box onClick={handleClick} cells={cells}/>
     </div>
   );
 }
