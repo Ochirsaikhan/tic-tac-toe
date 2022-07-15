29067bcad31ae4c60e1bc2df6b98565dd13a09c8
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-07-15 10:06:55 -0400

    Change the point color

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,8 +2,8 @@ export const Score = ({ winner }) => {
 
     return (
         <div className="score">
-            <h2>X points: {winner["X"]}</h2>
-            <h2>O points: {winner["O"]}</h2>
+            <h2>X points: <span className="point">{winner["X"]}</span></h2>
+            <h2>O points: <span className="point">{winner["O"]}</span></h2>
         </div>
     );
 };
